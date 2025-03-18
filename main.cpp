b3088d82a83701d7d48d606720651bfe08b6b69b
Dodrio33 <josephshipleysmith@gmail.com>
2025-03-18 14:52:58 +0000

    Added Text To Binary

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <string>
 #include <fstream>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
@@ -12,6 +14,50 @@ static char convertToLower(string text) {
     char charToConvert = text[0];
     return tolower(charToConvert);
 };
+
+// Function to Translate binary to text
+
+static void convertToText() {
+    string binaryToCovert;
+
+    //recive Inputs
+    cout << "Enter the ASCII binary that you want to convert: ";
+    getline(cin, binaryToCovert);
+    cout << endl;
+
+    ifstream File(charSet);
+
+    //Remove Whitespace from the binary
+    binaryToCovert.erase(remove_if(binaryToCovert.begin(), binaryToCovert.end(), 
+    [](unsigned char c) { return std::isspace(c); }), binaryToCovert.end());
+
+    for (size_t i = 0; i < binaryToCovert.size(); i += 8) {
+        string c = binaryToCovert.substr(i, 8);
+
+        string Line;
+        string Text;
+        string Binary;
+        
+        File.clear();
+        File.seekg(0, ios::beg);
+
+        while (getline(File, Line)) {
+            stringstream StringStream(Line);
+
+            getline(StringStream, Text, ',');
+            getline(StringStream, Binary, ',');
+
+            if (Binary == c) {
+                cout << Text;
+                break;
+            }
+        }
+    }
+
+    cout << endl;
+    
+}
+// Function to Translate to binary
 
 static void convertToBinary() {
     // Function to Translate to binary
@@ -64,7 +110,7 @@ int main() {
                 convertToBinary();
                 return 0;
             case 't':
-                cout << "Not implemented!";
+                convertToText();
                 return 0;
             default:
                 continue;
